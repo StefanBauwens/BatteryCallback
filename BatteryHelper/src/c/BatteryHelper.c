dd1018bf98de3aa76b540abd99cecc9575af0cfc
Stefan Bauwens <stefan.bauwens@hotmail.com>
2025-03-07 20:55:29 +0100

    Remove endpoint since we don't need in from the watch side + Add code to see battery level

--- BatteryHelper/src/c/BatteryHelper.c
+++ BatteryHelper/src/c/BatteryHelper.c
@@ -1,10 +1,7 @@
 #define SETTINGS_KEY 1
-#define URL_MAX_LENGTH 80 //TODO unused for now
 #include <pebble.h>
-//#include <stdio.h>
 
 typedef struct ClaySettings {
-  char *Endpoint; //TODO test
   bool SendWhenAppOpened;
   bool SendWhenBatteryChanged;
   bool SendAtFixedTime;
@@ -18,7 +15,6 @@ static TextLayer *s_text_layer;
 
 // Initialize the default settings
 static void prv_default_settings() {
-  settings.Endpoint = "";
   settings.SendWhenAppOpened = true;
   settings.SendWhenBatteryChanged = false;
   settings.SendAtFixedTime = false;
@@ -57,14 +53,18 @@ bool parse_time(const char *time_str, int8_t *hours, int8_t *minutes) {
     return true;
 }
 
+static void QuitSelf()
+{
+  window_stack_pop_all(true);
+}
+
 // AppMessage receive handler
 static void prv_inbox_received_handler(DictionaryIterator *iter, void *context) {
-  
   // Assign the values to our struct
-  Tuple *endpoint_t = dict_find(iter, MESSAGE_KEY_endpoint);
+  /*Tuple *endpoint_t = dict_find(iter, MESSAGE_KEY_endpoint);
   if (endpoint_t) {
     settings.Endpoint = endpoint_t->value->cstring;
-  }
+  }*/
   
   Tuple *sendWhenAppOpened_t = dict_find(iter, MESSAGE_KEY_sendWhenAppOpened);
   if (sendWhenAppOpened_t) {
@@ -100,23 +100,11 @@ static void prv_inbox_received_handler(DictionaryIterator *iter, void *context) 
   prv_save_settings();
 }
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
-  window_stack_pop_all(false);
-}
-
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
-}
-
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
-}
-
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
+static void prv_get_battery() {
+  BatteryChargeState state = battery_state_service_peek();
+  char battery_text[5];
+  snprintf(battery_text, sizeof(battery_text), "%d", state.charge_percent);
+  text_layer_set_text(s_text_layer, battery_text);
 }
 
 static void prv_window_load(Window *window) {
@@ -124,9 +112,10 @@ static void prv_window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
+  text_layer_set_text(s_text_layer, "Fetching battery...");
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  prv_get_battery();
 }
 
 static void prv_window_unload(Window *window) {
@@ -141,7 +130,6 @@ static void prv_init(void) {
   app_message_open(128, 128);
 
   s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
   window_set_window_handlers(s_window, (WindowHandlers) {
     .load = prv_window_load,
     .unload = prv_window_unload,
