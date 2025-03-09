ded6d8a29efc558600cd5603fc14ee8e9ea9219d
Stefan Bauwens <stefan.bauwens@hotmail.com>
2025-03-09 20:38:26 +0100

    Make worker not send redundant calls

--- BatteryCallback/worker_src/c/worker.c
+++ BatteryCallback/worker_src/c/worker.c
@@ -1,11 +1,33 @@
+#define LAST_STATE_KEY 2
 #include <pebble_worker.h>
 
+static BatteryChargeState s_prev_charge_state;
+
 static void handle_battery(BatteryChargeState charge_state) {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Battery change event!");
-  worker_launch_app(); // call the main app
+  if (persist_exists(LAST_STATE_KEY)) {
+    // Read settings from persistent storage
+    persist_read_data(LAST_STATE_KEY, &s_prev_charge_state, sizeof(s_prev_charge_state));
+
+    if(s_prev_charge_state.charge_percent != charge_state.charge_percent || s_prev_charge_state.is_charging != charge_state.is_charging || s_prev_charge_state.is_plugged != charge_state.is_plugged) { 
+      // write changes
+      s_prev_charge_state = charge_state;
+      persist_write_data(LAST_STATE_KEY, &,s_prev_charge_state sizeof(s_prev_charge_state));
+      
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "Battery change event!");
+      worker_launch_app(); // call the main app
+    }
+  } else {
+    // write changes
+    s_prev_charge_state = charge_state;
+    persist_write_data(LAST_STATE_KEY, &,s_prev_charge_state sizeof(s_prev_charge_state));
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Battery change event!");
+    worker_launch_app(); // call the main app
+  }
 }
 
 static void prv_init() {
+  s_prev_charge_state = battery_state_service_peek(); // get default value
   battery_state_service_subscribe(handle_battery);
 }
 
